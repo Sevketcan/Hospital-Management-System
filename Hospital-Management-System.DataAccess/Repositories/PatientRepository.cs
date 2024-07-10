97671c316b2592d357877545cbf850402025a2ee
Sevketcan <sevketcan.kalyoncuoglu@std.yeditepe.edu.tr>
2024-07-10 18:42:49 +0300

    Repo Created

---
+++ Hospital-Management-System.DataAccess/Repositories/PatientRepository.cs
@@ -0,0 +1,40 @@
+﻿using Hospital_Management_System.DataAccess.Contexts;
+using Hospital_Management_System.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System.DataAccess.Repositories
+{
+    public class PatientRepository
+    {
+        HospitalDbContext _context = new HospitalDbContext();  //veritabanı
+
+        public List<Patient> GetAll()
+        {
+            return _context.Patients.ToList();
+        }
+        public Patient GetById(int id)
+        {
+            return _context.Patients.Find(id);
+        }
+        public void Add(Patient patient)
+        {
+            _context.Patients.Add(patient);  //Ara katmana ekler.
+            _context.SaveChanges();         //veritabanıyla eşleştirir.
+        }
+        public void Update(Patient patient)
+        {
+            _context.Patients.Update(patient);
+            _context.SaveChanges();
+        }
+        public void Delete(Patient patient)
+        {
+            _context.Patients.Remove(patient);
+            _context.SaveChanges();
+        }
+    }
+}
---
+++ Hospital-Management-System.DataAccess/Repositories/PrescriptionRepository.cs
@@ -0,0 +1,40 @@
+﻿using Hospital_Management_System.DataAccess.Contexts;
+using Hospital_Management_System.Entity.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital_Management_System.DataAccess.Repositories
+{
+    public class PrescriptionRepository
+    {
+        HospitalDbContext _context = new HospitalDbContext();  //veritabanı
+
+        public List<Prescription> GetAll()
+        {
+            return _context.Prescriptions.ToList();
+        }
+        public Prescription GetById(int id)
+        {
+            return _context.Prescriptions.Find(id);
+        }
+        public void Add(Prescription prescription)
+        {
+            _context.Prescriptions.Add(prescription);  //Ara katmana ekler.
+            _context.SaveChanges();         //veritabanıyla eşleştirir.
+        }
+        public void Update(Prescription prescription)
+        {
+            _context.Prescriptions.Update(prescription);
+            _context.SaveChanges();
+        }
+        public void Delete(Prescription prescription)
+        {
+            _context.Prescriptions.Remove(prescription);
+            _context.SaveChanges();
+        }
+    }
+}
